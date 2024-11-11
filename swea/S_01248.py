4143f6ceab84eb214dcd01e4f70aad22f67f9d6d
Macbook Air M1 <macbookairm1@Macbookui-MacBookAir.local>
2024-11-12 02:20:27 +0900

    공통조상-미완성

--- swea/S_01248.py
+++ swea/S_01248.py
@@ -2,16 +2,20 @@ import sys
 sys.stdin = open('S_01248.txt')
 
 # DFS를 활용한 길 찾기
-def bfs(v,path,target):
+def dfs(v,path,target):
+    global global_path
+    visited[v] = 1
+    path.append(v)
+    if v == target:
+        global_path = path[:]
 
-
-
-
-
-
+    for w in tree[v]:
+        if not visited[w]:
+            dfs(w,path,target)
+            path.pop()
 
 T = int(input())
-for test_case in range(1,T+1):
+for test_case in range(1,2):
     V, E, X, Y = map(int,input().split())       # V: 정점의 수, E: 간선의 수, X,Y: 공통조상을 찾는 두 정점
     edges = list(map(int,input().split()))
     tree = [[] for _ in range(V+1)]
@@ -21,3 +25,11 @@ for test_case in range(1,T+1):
         tree[u].append(v)
 
     visited = [0] * (V+1)
+    global_path = []
+
+    dfs(1,[],X)
+    path_x = global_path[:]
+    dfs(1,[],Y)
+    path_y = global_path[:]
+    print(path_x)
+    print(path_y)
